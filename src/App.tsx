cc48ffaeb7419f7e6799781d03bee076b1f8f098
Malith Ranasinghe <ranasinghemalith20@gmail.com>
2026-02-03 17:36:33 +0530

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/login/Login';
-import './App.css';
-import UserDetailsPage from './pages/userDetailsPage/UserDetailsPage';
 import UsersPage from './pages/usersPage/UsersPage';
+import UserDetailsPage from './pages/userDetailsPage/UserDetailsPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/users/:id" element={<UserDetailsPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
